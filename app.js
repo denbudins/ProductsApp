2d982e2122610fa0814f9bd2dcd80de85b829420
Denis Martin Budinski <denis.budinski@endcoding.hr>
2021-02-16 13:44:45 +0100

    Finished app

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const removeButton = document.getElementById("removeAllProduct");
 var products = new Array();
 
 addButton.addEventListener("click", () => {
-  const newProduct = document.getElementById("productName").value;
+  const newProduct = document.getElementById("productName");
   const list = document.getElementById("listOfProduct");
   var listOfProduct = "";
 
-  products.push(newProduct);
+  products.push(newProduct.value);
   products.forEach((product) => {
     listOfProduct += `<li>${product}</li>`;
   });
@@ -15,13 +15,19 @@ addButton.addEventListener("click", () => {
   document.getElementById(
     "numberOfProducts"
   ).innerText = `Number of products: ${products.length}`;
+  newProduct.value = "";
 });
 
 removeButton.addEventListener("click", () => {
+  var r = confirm(
+    "Are you sure that you want to remove all products from list?"
+  );
   const list = document.getElementById("listOfProduct");
-  products = [];
-  list.innerHTML = "";
-  document.getElementById(
-    "numberOfProducts"
-  ).innerText = `Number of products: ${products.length}`;
+  if (r == true) {
+    products = [];
+    list.innerHTML = "";
+    document.getElementById(
+      "numberOfProducts"
+    ).innerText = `Number of products: ${products.length}`;
+  }
 });
